034e7b61b5c67daa543842ee95b8a4133a388537
sunset <megachj@gmail.com>
2024-04-10 23:38:50 +0900

    feat: 예제 하위 프로젝트들 생성

---
+++ independent/example-independent/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm")
+}
+
+group = "sunset"
+version = "1.0.0"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
